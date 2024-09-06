b560936b6135fdd17e5d921299de3f29682503d0
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-06 10:03:49 +0200

    Add files via upload

---
+++ Up and down counter/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Write("Mata in ett stort och ett litet tal : ");
+
+string input = Console.ReadLine();
+
+string[] numberString = input.Split(' ');
+
+
+
+if(numberString.Length == 2 )
+{
+    int a = int.Parse(numberString[0]);
+    int b = int.Parse(numberString[1]);
+
+    if(a < b)
+    {
+        Console.WriteLine("Räkna upp från " + a + " till " + b + ":");
+
+        for(int i = a; i <= b; i++)
+        {
+            Console.WriteLine(i); 
+        }
+    }
+    else if(a > b)
+    {
+        for(int i = a; i >= b;i--)
+        {
+            Console.WriteLine(i);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Talen är lika finns inget att räkna på");
+    }
+
+}
+else
+{
+    Console.WriteLine("Fel input, mata in exakt två tal");
+}
+
+
+
